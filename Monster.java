8f941ed453d00e91a757e5b7db88389d9d6e585d
unknown <vor1597@gmail.com>
2014-04-11 13:20:46 -0700

    Refactored Moveable, added Monster

--- Monster.java
+++ Monster.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
 /**
  * Write a description of class Monster here.
@@ -15,5 +16,45 @@ public class Monster extends Moveable
     public void act() 
     {
         // Add your action code here.
+        Hero hero = (Hero) getWorld().getObjects(Hero.class).get(0);
+        int heroX = hero.getX();
+        int heroY = hero.getY();
+        
+        int myX = getX();
+        int myY = getY();
+        
+        if (heroX - myX > 0) //East
+        {
+            setDirection(Moveable.EAST);
+            
+        }
+        else if(heroX - myX < 0)
+            setDirection(Moveable.WEST);
+        if (heroX - myX != 0 && canMove())
+        {
+            move();
+            return;
+        }
+        
+        if (heroY - myY > 0) //SOUTH
+        {
+            setDirection(Moveable.SOUTH);
+            
+        }
+        else if(heroY - myY < 0)
+            setDirection(Moveable.NORTH); 
+        if (heroY - myY != 0 && canMove())
+        {
+            move();
+            return;
+        }
+        
+        move();
     }    
+    
+    public Monster()
+    {
+        setDirection(Moveable.EAST);
+        setImage("images/monster.png");
+    }
 }
--- Moveable.java
+++ Moveable.java
@@ -12,10 +12,10 @@ import java.util.ArrayList;
  */
 public abstract class Moveable extends Actor
 {
-    private static final int EAST = 0;
-    private static final int WEST = 1;
-    private static final int NORTH = 2;
-    private static final int SOUTH = 3;
+    protected static final int EAST = 0;
+    protected static final int WEST = 1;
+    protected static final int NORTH = 2;
+    protected static final int SOUTH = 3;
 
     private int direction;
     
